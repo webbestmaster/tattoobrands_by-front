d40d064c0e722bbf00423de4dff41fb1647781ad
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2017-09-01 10:24:41 +0300

    add fast click

--- www/js/common.js
+++ www/js/common.js
@@ -2,4 +2,5 @@
 // if you know better way, please, talk to me. BR, Dmitry Turvotsov
 
 require('./lib/idangerous.swiper');
+require('fastclick');
 // require('jbone');
--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,11 @@
+/* global document */
 require('./../style/css/_root.scss');
+
+const FastClick = require('fastclick');
 
 const homeScripts = require('./home');
 
 window.addEventListener('load', () => {
+    new FastClick(document.body); // eslint-disable-line no-new
     homeScripts.initSwiper();
 });
