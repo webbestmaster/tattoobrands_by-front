e283bfefd7138cb6a08627e864f38c87af8236b1
dmitry.turovtsov <dmitry.turovtsov@synesis.ru>
2017-09-24 19:28:07 +0300

    category tree - in progress...

--- www/js/header-nav.js
+++ www/js/header-nav.js
@@ -2,6 +2,7 @@
 const React = require('react');
 const {Component} = React;
 const ReactDOM = require('react-dom');
+const find = require('lodash/find');
 // const {browser} = require('./my-lib/browser');
 
 window.app = window.app || {};
@@ -10,10 +11,17 @@ class TabContent extends Component {
     render() {
         const view = this;
         const {props} = view;
-        const {content} = props;
+        const {category} = props;
+        const {categories} = category;
 
-        return <div className="header-nav__tab-content">
-
+        return <div className="header-nav__tab-wrapper">
+            <div className="width-limiter">
+                {categories.map(({image, displayName, name, slug}) =>
+                    <a href={'/category/' + slug} className="category-item">
+                        <h3>{displayName || name}</h3>
+                        <img src={image} alt={displayName || name}/>
+                    </a>)}
+            </div>
         </div>;
     }
 }
@@ -27,23 +35,43 @@ class HeaderNav extends Component {
         const {categoryTree} = app;
 
         view.state = {
-            categoryTree: JSON.parse(JSON.stringify(categoryTree))
+            categoryTree: JSON.parse(JSON.stringify(categoryTree)),
+            activeCategorySlug: null
         };
+    }
+
+    openTab(slug) {
+        const view = this;
+
+        view.setState({activeCategorySlug: slug});
+    }
+
+    closeTabs() {
+        const view = this;
+
+        view.setState({activeCategorySlug: null});
     }
 
     render() {
         const view = this;
         const {state} = view;
         const {categoryTree} = state;
+        const activeCategory = find(categoryTree.categories, {slug: state.activeCategorySlug}) ||
+            categoryTree.categories.sort((category1, category2) => category1.order - category2.order)[0];
 
         return <div>
             {categoryTree.categories
                 .sort((category1, category2) => category1.order - category2.order)
-                .map(category =>
-                    <div key={category.slug} className="header-nav__link">
-                        {category.displayName || category.name}
-                    </div>)
+                .map(({displayName, name, slug}) => <a
+                    href={'/category/slug'}
+                    onMouseEnter={() => view.openTab(slug)}
+                    onMouseLeave={() => view.closeTabs()}
+                    key={slug} className="header-nav__link">
+                    {displayName || name}
+                </a>)
             }
+
+            {activeCategory && <TabContent category={activeCategory}/>}
         </div>;
     }
 }
